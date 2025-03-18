8e4f9609f0063069aad76e000b0bdeda2222fcf1
carimvch33 <CarimVel2004@gmail.com>
2025-03-17 21:58:08 -0600

    Implementación de los Casos de Uso 1 (Registrar Producto) y 2 (Editar Producto), creación del DAO para Producto, con los metodos para registrar, obtener y actualizar datos, implementación del mapeo de la tabla producto en MelodiasContext, migración para Producto, creación de su DataContract, y su respectiva interfaz de servicio junto con la implementación.

---
+++ DataAccess/Migrations/202503172134574_TablaParaProductos.cs
@@ -0,0 +1,38 @@
+﻿namespace DataAccess.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class TablaParaProductos : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Products",
+                c => new
+                    {
+                        ProductId = c.Int(nullable: false, identity: true),
+                        ProductName = c.String(nullable: false, maxLength: 100),
+                        ProductCode = c.String(nullable: false, maxLength: 50),
+                        Description = c.String(maxLength: 500),
+                        PurchasePrice = c.Decimal(nullable: false, precision: 10, scale: 2),
+                        SalePrice = c.Decimal(nullable: false, precision: 10, scale: 2),
+                        Category = c.String(nullable: false, maxLength: 50),
+                        Brand = c.String(nullable: false, maxLength: 50),
+                        Model = c.String(maxLength: 50),
+                        Stock = c.Int(nullable: false),
+                        Photo = c.String(maxLength: 255),
+                        Status = c.Boolean(nullable: false),
+                    })
+                .PrimaryKey(t => t.ProductId)
+                .Index(t => t.ProductName, unique: true);
+            
+        }
+        
+        public override void Down()
+        {
+            DropIndex("dbo.Products", new[] { "ProductName" });
+            DropTable("dbo.Products");
+        }
+    }
+}
