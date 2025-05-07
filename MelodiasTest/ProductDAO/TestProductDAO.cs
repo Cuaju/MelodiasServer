29e297b3b78361b472e29a3a134e02dc54931920
carimvch33 <CarimVel2004@gmail.com>
2025-05-07 08:44:27 -0600

    CORREGÍ MIS PRUEBAS DE PRODUCTO

--- MelodiasTest/ProductDAO/TestProductDAO.cs
+++ MelodiasTest/ProductDAO/TestProductDAO.cs
@@ -1,215 +1,135 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
-using System.Linq;
-using System.ServiceModel;
-using Xunit;
-using DataAccess;
+﻿using DataAccess;
 using DataAccess.DAO;
 using DataAccess.Models;
+using System;
+using System.Linq;
+using System.ServiceModel;
+using System.Transactions;
+using Xunit;
 
 namespace MelodiasTests.ProductDAO
 {
-    public class TestProductDao : IDisposable
+    public class TestProductDao
     {
-        private readonly MelodiasContext _context;
-        private readonly DbContextTransaction _transaction;
-        private readonly ProductDao _dao;
-        private int _productId;
-
-        public TestProductDao()
+        private Product CreateValidProduct(string suffix, bool hasSales = false)
         {
-            _context = new MelodiasContext();
-            _transaction = _context.Database.BeginTransaction();
-            _dao = new ProductDao();
-            InicializarProductoPrueba();
-        }
-
-        private void InicializarProductoPrueba()
-        {
-            var producto = new Product
+            return new Product
             {
-                ProductName = "ProductoTest",
-                ProductCode = "PTEST",
-                Description = "Producto de prueba",
+                ProductName = $"Prod_{suffix}",
+                ProductCode = $"PC_{suffix}",
+                Description = "Description",
                 PurchasePrice = 10,
                 SalePrice = 15,
-                Category = "Accesorios",
-                Brand = "MarcaTest",
-                Model = "ModeloX",
-                Stock = 10,
-                Photo = "img.jpg",
-                Status = true,
-                HasSales = false
-            };
-
-            _context.Products.Add(producto);
-            _context.SaveChanges();
-            _productId = producto.ProductId;
-        }
-
-        [Fact]
-        public void RegisterProduct_Success()
-        {
-            var nuevo = new Product
-            {
-                ProductName = "NuevoProducto",
-                Description = "Desc",
-                PurchasePrice = 5,
-                SalePrice = 8,
-                Category = "Otros",
-                Brand = "MarcaNueva",
-                Model = "M1",
-                Stock = 3,
-                Photo = "img.png",
-                Status = true,
-                HasSales = false
-            };
-
-            var result = _dao.RegisterProduct(nuevo);
-            Assert.True(result);
-        }
-
-        [Fact]
-        public void RegisterProduct_Fail_DuplicatedName()
-        {
-            var duplicado = new Product
-            {
-                ProductName = "ProductoTest",
-                ProductCode = "P12345"
-            };
-
-            var ex = Assert.Throws<FaultException>(() => _dao.RegisterProduct(duplicado));
-            Assert.Contains("A product with this name already exists.", ex.Message);
-        }
-
-        [Fact]
-        public void GetProducts_ReturnsActiveProducts()
-        {
-            var productos = _dao.GetProducts();
-            Assert.NotEmpty(productos);
-            Assert.All(productos, p => Assert.True(p.Status));
-        }
-
-        [Fact]
-        public void UpdateProduct_Success()
-        {
-            var modificado = new Product
-            {
-                ProductId = _productId,
-                ProductName = "ProductoTestModificado",
-                Description = "Modificado",
-                PurchasePrice = 12,
-                SalePrice = 18,
-                Category = "Nueva",
-                Brand = "Otra",
-                Model = "Nuevo",
-                Stock = 20,
-                Photo = "mod.jpg",
-                Status = true
-            };
-
-            var result = _dao.UpdateProduct(modificado);
-            Assert.True(result);
-        }
-
-        [Fact]
-        public void UpdateProduct_Fail_DuplicatedName()
-        {
-            var otro = new Product
-            {
-                ProductName = "OtroNombre",
-                Description = "Desc",
-                PurchasePrice = 1,
-                SalePrice = 2,
-                Category = "Cat",
+                Category = "Category",
                 Brand = "Brand",
                 Model = "Model",
-                Stock = 5,
-                Photo = "",
-                Status = true,
-                HasSales = false
-            };
-
-            _context.Products.Add(otro);
-            _context.SaveChanges();
-
-            var duplicado = new Product
-            {
-                ProductId = _productId,
-                ProductName = "OtroNombre"
-            };
-
-            var ex = Assert.Throws<FaultException>(() => _dao.UpdateProduct(duplicado));
-            Assert.Contains("already exists", ex.Message);
-        }
-
-        [Fact]
-        public void DeleteProduct_Success()
-        {
-            var resultado = _dao.DeleteProduct(_productId);
-            Assert.True(resultado);
-        }
-
-        [Fact]
-        public void DeleteProduct_Fail_HasSales()
-        {
-            var vendido = new Product
-            {
-                ProductName = "Vendido",
-                Description = "Ya fue vendido",
-                PurchasePrice = 20,
-                SalePrice = 25,
-                Category = "Eliminación",
-                Brand = "TestBrand",
-                Model = "X1",
                 Stock = 10,
-                Photo = "vendido.jpg",
+                Photo = "photo.jpg",
                 Status = true,
-                HasSales = true
+                HasSales = hasSales
             };
-
-            _context.Products.Add(vendido);
-            _context.SaveChanges();
-
-            var ex = Assert.Throws<FaultException>(() => _dao.DeleteProduct(vendido.ProductId));
-            Assert.Contains("no puede ser eliminado", ex.Message);
-        }
-
-
-        [Fact]
-        public void ExistsProductByName_ReturnsTrue()
-        {
-            var existe = _dao.ExistsProductByName("ProductoTest", _productId + 1000);
-            Assert.True(existe);
         }
 
         [Fact]
-        public void ExistsProductByName_ReturnsFalse()
+        public void DeleteProduct_ReturnsTrue_WhenNoSales()
         {
-            var existe = _dao.ExistsProductByName("Inexistente", 999);
-            Assert.False(existe);
+            using (var scope = new TransactionScope())
+            {
+                int id;
+                using (var ctx = new MelodiasContext())
+                {
+                    var prod = CreateValidProduct(Guid.NewGuid().ToString("N"));
+                    ctx.Products.Add(prod);
+                    ctx.SaveChanges();
+                    id = prod.ProductId;
+                }
+
+                var dao = new ProductDao();
+                var result = dao.DeleteProduct(id);
+                Assert.True(result);
+            }
         }
 
         [Fact]
-        public void GetProductsList_ByName()
+        public void DeleteProduct_ThrowsFaultException_WhenHasSales()
         {
-            var lista = _dao.GetProductsList("ProductoTest");
-            Assert.NotEmpty(lista);
+            using (var scope = new TransactionScope())
+            {
+                int id;
+                using (var ctx = new MelodiasContext())
+                {
+                    var prod = CreateValidProduct(Guid.NewGuid().ToString("N"), true);
+                    ctx.Products.Add(prod);
+                    ctx.SaveChanges();
+                    id = prod.ProductId;
+                }
+
+                var dao = new ProductDao();
+                Assert.Throws<FaultException>(() => dao.DeleteProduct(id));
+            }
         }
 
         [Fact]
-        public void SearchProducts_WithFilters()
+        public void DeleteProduct_ThrowsFaultException_WhenNotFound()
         {
-            var resultados = _dao.SearchProducts("ProductoTest", "PTEST", "Accesorios", "MarcaTest");
-            Assert.NotEmpty(resultados);
+            var dao = new ProductDao();
+            Assert.Throws<FaultException>(() => dao.DeleteProduct(-999));
         }
 
-        public void Dispose()
+        [Fact]
+        public void ExistsProductByName_ReturnsTrue_WhenDuplicateExists()
         {
-            _transaction.Rollback();
-            _transaction.Dispose();
-            _context.Dispose();
+            using (var scope = new TransactionScope())
+            {
+                var suffix = Guid.NewGuid().ToString("N");
+                using (var ctx = new MelodiasContext())
+                {
+                    var prod = CreateValidProduct(suffix);
+                    ctx.Products.Add(prod);
+                    ctx.SaveChanges();
+                }
+
+                var dao = new ProductDao();
+                Assert.True(dao.ExistsProductByName($"Prod_{suffix}", -1));
+            }
+        }
+
+        [Fact]
+        public void ExistsProductByName_ReturnsFalse_WhenNameIsUnique()
+        {
+            var dao = new ProductDao();
+            Assert.False(dao.ExistsProductByName("UniqueName_" + Guid.NewGuid(), -1));
+        }
+
+        [Fact]
+        public void GetProductsList_ReturnsEmpty_WhenNoMatch()
+        {
+            using (var scope = new TransactionScope())
+            {
+                var dao = new ProductDao();
+                var result = dao.GetProductsList("no_match_12345");
+                Assert.Empty(result);
+            }
+        }
+
+        [Fact]
+        public void GetProductsList_ReturnsMatchingProducts()
+        {
+            using (var scope = new TransactionScope())
+            {
+                var token = Guid.NewGuid().ToString("N").Substring(0, 5);
+                using (var ctx = new MelodiasContext())
+                {
+                    ctx.Products.Add(CreateValidProduct("A" + token));
+                    ctx.Products.Add(CreateValidProduct("B" + token));
+                    ctx.SaveChanges();
+                }
+
+                var dao = new ProductDao();
+                var result = dao.GetProductsList(token);
+                Assert.True(result.Count >= 2);
+                Assert.All(result, p => Assert.Contains(token, p.ProductName));
+            }
         }
     }
 }
